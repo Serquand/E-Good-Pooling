a2c7d58e3428ca54b3e2cbe6096b051e7435710c
Serquand <=>
2022-11-07 03:13:37 +0100

    Setup client routes + beginning Login

--- client/App.tsx
+++ client/App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Login from './views/Login';
+import { NavigationContainer as Nav } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const {Navigator, Screen} = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Nav>   
+            <Navigator initialRouteName='Login'>
+                <Screen name='Login' component={Login} options={{ headerShown: false }} />
+            </Navigator>
+        </Nav>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ client/views/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, TextInput, View, Image, Pressable } from 'react-native';
+
+const Login = () => {
+    return (
+        <View>
+            <Text>Hello, World !</Text>
+        </View>
+    )
+}
+
+export default Login;
