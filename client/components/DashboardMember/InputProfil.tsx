d8b00ed8f9bd8f44a3746df734ecffec1a633c8a
Serquand <=>
2022-11-10 10:08:25 +0100

    Created files for AnnonceWall, InputProfil, and DashboardMember

---
+++ client/components/DashboardMember/InputProfil.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { TextInput, View, StyleSheet, Pressable } from 'react-native';
+
+type InputProfilProps = {
+    information: string;
+    handler: any;
+}
+
+const InputProfil = (props: InputProfilProps) => {
+    const { information, handler } = props;
+    
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default InputProfil;
